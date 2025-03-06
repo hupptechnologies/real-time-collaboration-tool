e8f97d211f2100d33865eced6f6c4fef01fafd54
hupptechnologies <git@hupp.in>
2025-03-06 11:57:55 +0530

    Remove Web Pack For Build Failed

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from 'next';
-import type { Configuration, RuleSetRule } from 'webpack';
 
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
-	webpack: (config: Configuration) => {
-		if (config.module !== null) {
-			config.module = { rules: [] };
-		}
-
-		(config.module.rules as RuleSetRule[]).push({
-			test: /\.svg$/,
-			use: ['@svgr/webpack']
-		});
-
-		return config;
-	},
 	devIndicators: false,
 	experimental: {
 		typedRoutes: true
