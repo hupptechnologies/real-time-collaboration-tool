6c323daa9311ac652680292ff68269cf5938a087
hupptechnologies <git@hupp.in>
2025-03-11 17:16:54 +0530

    Fix Login Page With API

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,23 +1,101 @@
 'use client';
 import React, { useLayoutEffect } from 'react';
-import { Stack } from '@mui/material';
+import {
+	Stack,
+	Box,
+	Button,
+	Card,
+	FormLabel,
+	FormControl,
+	Link,
+	TextField,
+	Typography
+} from '@mui/material';
 import { redirect } from 'next/navigation';
+import { useFormik } from 'formik';
 import { useAuth } from '@/context/AuthContext';
-import LoginForm from '@/components/LoginForm';
-import { MainStackStyle, StackNextStyle, StackStyle } from '@/styles';
+import { IFormValues } from '@/types';
+import { loginValidationSchema } from '@/utils/validation';
+import { MainStackStyle, StackNextStyle, StackStyle, cardStyles, emailBoxStyles } from '@/styles';
 
 const Login: React.FC = () => {
-	const { user } = useAuth();
+	const { user, login } = useAuth();
 	useLayoutEffect(() => {
 		if (user) {
 			redirect('/dashboard');
 		}
 	}, []);
+
+	const formik = useFormik<IFormValues>({
+		initialValues: { email: '', password: '' },
+		validationSchema: loginValidationSchema,
+		onSubmit: async (values: any) => {
+			await login(values.email, values.password);
+		}
+	});
 	return (
 		<Stack direction="column" component="main" sx={MainStackStyle}>
 			<Stack direction={{ xs: 'column-reverse', md: 'row' }} sx={StackStyle}>
 				<Stack direction={{ xs: 'column-reverse', md: 'row' }} sx={StackNextStyle}>
-					<LoginForm />
+					<Card sx={cardStyles} variant="outlined">
+						<Typography
+							component="h1"
+							variant="h4"
+							sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}>
+							Sign In
+						</Typography>
+						<Box component="form" onSubmit={formik.handleSubmit} noValidate sx={emailBoxStyles}>
+							<FormControl>
+								<FormLabel htmlFor="email">Email</FormLabel>
+								<TextField
+									value={formik.values.email}
+									onChange={formik.handleChange}
+									error={formik.touched.email && !!formik.errors.email}
+									helperText={formik.touched.email && formik.errors.email}
+									id="email"
+									type="email"
+									name="email"
+									placeholder="your@email.com"
+									autoComplete="email"
+									autoFocus
+									required
+									fullWidth
+									variant="outlined"
+									color={formik.touched.email && !!formik.errors.email ? 'error' : 'primary'}
+								/>
+							</FormControl>
+							<FormControl>
+								<FormLabel htmlFor="password">Password</FormLabel>
+								<TextField
+									value={formik.values.password}
+									onChange={formik.handleChange}
+									error={formik.touched.password && !!formik.errors.password}
+									helperText={formik.touched.password && formik.errors.password}
+									name="password"
+									placeholder="••••••"
+									type="password"
+									id="password"
+									autoComplete="current-password"
+									autoFocus
+									required
+									fullWidth
+									variant="outlined"
+									color={formik.touched.password && !!formik.errors.password ? 'error' : 'primary'}
+								/>
+							</FormControl>
+							<Button type="submit" fullWidth variant="contained">
+								Sign in
+							</Button>
+							<Typography sx={{ textAlign: 'center' }}>
+								Don&apos;t have an account?{' '}
+								<span>
+									<Link href="/signup" variant="body2" sx={{ alignSelf: 'center' }}>
+										Sign up
+									</Link>
+								</span>
+							</Typography>
+						</Box>
+					</Card>
 				</Stack>
 			</Stack>
 		</Stack>
