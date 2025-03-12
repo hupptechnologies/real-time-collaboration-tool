3e992a4ee2c3b332653a27b704323223c54caf8f
hupptechnologies <git@hupp.in>
2025-03-12 11:05:33 +0530

    [FE] - Added Not Found Page

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,34 @@
+'use client';
+import { Box, Typography, Button } from '@mui/material';
+import { useRouter } from 'next/navigation';
+
+const NotFoundPage = () => {
+	const router = useRouter();
+	const handleGoHome = () => router.push('/');
+	return (
+		<Box
+			display="flex"
+			flexDirection="column"
+			alignItems="center"
+			justifyContent="center"
+			height="100vh"
+			textAlign="center"
+			bgcolor="background.default"
+			p={3}>
+			<Typography variant="h1" color="primary" gutterBottom>
+				404
+			</Typography>
+			<Typography variant="h5" gutterBottom>
+				Oops! The page you’re looking for doesn’t exist.
+			</Typography>
+			<Typography variant="body1" color="text.secondary" mb={3}>
+				It might have been moved or deleted.
+			</Typography>
+			<Button variant="contained" color="primary" onClick={handleGoHome}>
+				Go to Homepage
+			</Button>
+		</Box>
+	);
+};
+
+export default NotFoundPage;
